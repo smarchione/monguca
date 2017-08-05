c3ccb364c53f66a2a3e5fe7913f81f81dc9120e5
Santiago Scalise <santiago@developeando.com.ar>
2017-08-04 21:41:32 -0300

    retornamos json

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+let URL = "mongodb://localhost:27017";
 let express = require("express");
 let path = require("path");
+let mongo = require("mongodb").MongoClient;
 let app = express();
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "auto.html"));
+});
+
+app.get("/report", (req, res) => {
+	
+	mongo.connect(URL, (err, client) => {
+		let db = client.db("airport");
+		
+		let options = {
+			"limit": 5
+		};
+		
+		db.collection("flights").find({}, options).toArray((err, data) => {
+			console.log(data);
+			res.send({ "flights": data });
+		});
+	});	
 });
 
 app.use(express.static("public"));
